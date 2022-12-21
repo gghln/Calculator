f98573df388f9c1dafb1460dfa8fe9aa9f15fb2c
gghln <nikolasg95@gmail.com>
2022-12-21 16:01:51 +0200

    Finished opertions

--- index.js
+++ index.js
@@ -1,21 +1,77 @@
-//Eventlistener gia to click tou koumpiou kai emfanisi tou ari8mou sthn o8oni.
-addEventListener('click',(e)=>{
-    let value = e.target.innerText;
-    document.getElementById("display").innerText += value;
-    if(value == "+"){
-        //pairnw to string, afairw to sumvolo tis praxis, to metatrepw se int
-        firstNumber = document.getElementById("display").innerText;
-        firstNumber = firstNumber.replace(/\D/g,'')
-        firstNumber = parseInt(firstNumber);
-        operator = value;
-        document.getElementById("display").innerText = "";
-    }else if(value == "="){
-        secondNumber = document.getElementById("display").innerText;
-        secondNumber = secondNumber.replace(/\D/g,'')
-        secondNumber = parseInt(secondNumber);
-        operate(firstNumber, secondNumber, operator);
-    }
-});    
+//Πρόσθεσα window.EventListener για να ξεκινάει η εκτέλεση μετά τη φόρτωση της σελίδας.
+window.addEventListener('DOMContentLoaded', () => {
+    //Επιλέγει όλα τα elements με class numbers και operator. το buttons είναι array.
+    let buttons = document.querySelectorAll(".numbers, .operator")
+    // For each element in the array
+    buttons.forEach(item =>
+        //Eventlisterne για click σε κάθε κουμπί
+        item.addEventListener('click',(e)=>{
+            //το value παίρνει την τιμή του κουμιού.
+            //και το προσθέτει στο πεδίο display της οθόνης.
+            let value = e.target.innerText;
+            document.getElementById("display").innerText += value;
+
+            if(value == "+"){
+                //Ο πρώτος αριθμός της οθόνης αποθηκεύεται ως string.
+                firstNumber = document.getElementById("display").innerText;
+                //Αφαιρούμε τα σύμβολα των πράξεων από το string
+                firstNumber = firstNumber.replace(/\D/g,'')
+                //Μετατρέπουμε σε integer
+                firstNumber = parseInt(firstNumber);
+                //Αποθήκευση του operator
+                operator = value;
+                //Αδειάζουμε την οθόνη
+                document.getElementById("display").innerText = "";
+                document.getElementById("firstNumber").innerText = firstNumber;
+                document.getElementById("operator").innerText = operator;
+                
+            }else if(value == "-"){
+                firstNumber = document.getElementById("display").innerText;
+
+                firstNumber = firstNumber.replace(/\D/g,'')
+
+                firstNumber = parseInt(firstNumber);
+
+                operator = value;
+                document.getElementById("display").innerText = "";
+                document.getElementById("firstNumber").innerText = firstNumber;
+                document.getElementById("operator").innerText = operator;
+            }else if(value == "*"){
+                firstNumber = document.getElementById("display").innerText;
+
+                firstNumber = firstNumber.replace(/\D/g,'')
+
+                firstNumber = parseInt(firstNumber);
+
+                operator = value;
+                document.getElementById("display").innerText = "";
+                document.getElementById("firstNumber").innerText = firstNumber;
+                document.getElementById("operator").innerText = operator;
+            }else if(value == "/"){
+                firstNumber = document.getElementById("display").innerText;
+
+                firstNumber = firstNumber.replace(/\D/g,'')
+
+                firstNumber = parseInt(firstNumber);
+
+                operator = value;
+                document.getElementById("display").innerText = "";
+                document.getElementById("firstNumber").innerText = firstNumber;
+                document.getElementById("operator").innerText = operator;
+            }
+            else if(value == "="){
+                //αποθηκεύουμε τον δεύτερο αριθμό
+                secondNumber = document.getElementById("display").innerText;
+                //αφαιρούμε τα σύμβολα από το string
+                secondNumber = secondNumber.replace(/\D/g,'')
+                //μετατροπή σε integer
+                secondNumber = parseInt(secondNumber);
+                //κλήση συνάρτηση με τον πρώτο αριθμό, τον δεύτερο και το σύμβολο της πράξης
+                operate(firstNumber, secondNumber, operator);
+            }
+        })
+    )
+});
 
 //choose operator
 function operate(a,b,operand){
@@ -24,31 +80,28 @@ function operate(a,b,operand){
     }
     else if(operand == "-"){
         subtract(a,b);
-        console.log(subtract(a,b));
     }
     else if(operand == "*"){
         multiply(a,b);
-        console.log(multiply(a,b));
     }
     else{
         divide(a,b);
-        console.log(divide(a,b));
     }
 }
 
-// +
+// Add
 function add(a,b){
     document.getElementById("display").innerText = a + b;
 }
-// -
+// Subtract
 function subtract(a,b){
-    return a - b;
+    document.getElementById("display").innerText = a - b;
 }
-// *
+// Multiply
 function multiply(a,b){
-    return a * b;
+    document.getElementById("display").innerText = a * b;
 }
-// /
+// Divide
 function divide(a,b){
-    return a + b;
+    document.getElementById("display").innerText = a / b;
 }
