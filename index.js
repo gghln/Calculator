2c31f9fb747aefe8ac988ead2d5dae4a19d3f8fe
gghln <61983256+gghln@users.noreply.github.com>
2022-12-21 13:43:10 +0200

    Πρόσθεση δύο αριθμων

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-//let operand = prompt("Choose an operation");
-let a = 1;
-let b = 2;
+//Eventlistener gia to click tou koumpiou kai emfanisi tou ari8mou sthn o8oni.
+addEventListener('click',(e)=>{
+    let value = e.target.innerText;
+    document.getElementById("display").innerText += value;
+    if(value == "+"){
+        //pairnw to string, afairw to sumvolo tis praxis, to metatrepw se int
+        firstNumber = document.getElementById("display").innerText;
+        firstNumber = firstNumber.replace(/\D/g,'')
+        firstNumber = parseInt(firstNumber);
+        operator = value;
+        document.getElementById("display").innerText = "";
+    }else if(value == "="){
+        secondNumber = document.getElementById("display").innerText;
+        secondNumber = secondNumber.replace(/\D/g,'')
+        secondNumber = parseInt(secondNumber);
+        operate(firstNumber, secondNumber, operator);
+    }
+});    
 
 //choose operator
 function operate(a,b,operand){
     if(operand == "+"){
         add(a,b);
-        console.log(add(a,b));
     }
     else if(operand == "-"){
         subtract(a,b);
@@ -22,12 +36,9 @@ function operate(a,b,operand){
     }
 }
 
-//call of function
-operate(a,b,operand);
-
 // +
 function add(a,b){
-    return a + b;
+    document.getElementById("display").innerText = a + b;
 }
 // -
 function subtract(a,b){
@@ -41,4 +52,3 @@ function multiply(a,b){
 function divide(a,b){
     return a + b;
 }
-
